0249f1c641b77b7fa1d264a37d771293d0179960
A1T3FYAA <akash.masih@vlinkinfo.com>
2022-07-14 13:00:15 +0530

    Sturcutre is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import './app.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to bitbytes</h1>
-      <img src={logo} />
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
     </div>
   );
 }
