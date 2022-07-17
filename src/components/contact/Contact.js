0f381a512bb20fe5d532dc464b0bff52c7dc2e3c
A1T3FYAA <akash.masih@vlinkinfo.com>
2022-07-17 20:14:24 +0530

    UI complete of portfolio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import './contact.css'
+import { GrMail } from 'react-icons/gr'
+import { ImWhatsapp } from 'react-icons/im'
 
 function Contact() {
     return (
-        <section id="contact">Contact</section>
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <div className="contact__detail">
+                    <div className="contact__deatil-card">
+                        <GrMail className='conatct__icon' />
+                        <h3>Email</h3>
+                        <p>akashmasih155@gmail.com</p>
+                        <a target='_blank' href="mailto:akashmasih155@gmail.com">send a message</a>
+                    </div>
+                    <div className="contact__deatil-card">
+                        <ImWhatsapp className='conatct__icon' />
+                        <h3>Whatsapp</h3>
+                        <p>+9178888-46659</p>
+                        <a target='_blank' href="https://api.whatsapp.com/send?phone=7888846659">Send Message</a>
+                    </div>
+                </div>
+                <div className="contact__form">
+                    <form >
+                        <input type="text" placeholder='Your Full Name' name="name" />
+                        <input type="email" placeholder='Your Email' name="email" />
+                        <textarea name="message" rows="3" placeholder='Your Message' />
+                        <input type="submit" value="Send Message" className='btn btn-primary' />
+                    </form>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import './footer.css'
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <h2 className='brand'>AKASH MASIH</h2>
+            <div className="footer__link">
+                <a href="#">Home</a>
+                <a href="#about">About</a>
+                <a href="#experience">Experience</a>
+                <a href="#services">Services</a>
+                <a href="#portfolio">Portfolio</a>
+                <a href="#testimonials">Testimonials</a>
+                <a href="#contact">Contact</a>
+            </div>
+            <div className="footer__social">
+                <a target='_blank' href="">
+                    <FaFacebookF />
+                </a>
+                <a target='_blank' href="">
+                    <FaLinkedinIn />
+                </a>
+                <a target='_blank' href="">
+                    <FaTwitter />
+                </a>
+            </div>
+        </footer>
     )
 }
 
